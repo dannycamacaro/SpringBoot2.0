a5374366525b524ab9764fd93f01bc4ae85d9929
DESKTOP-F1DBC7V\Danny <DannyCamacaro>
2022-10-14 17:25:46 -0500

    Multiples ways to configure a bean

---
+++ chapter2/1-ConfigureABean/configurebean/src/test/java/com/camamonte/configurebean/ConfigurebeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camamonte.configurebean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurebeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
