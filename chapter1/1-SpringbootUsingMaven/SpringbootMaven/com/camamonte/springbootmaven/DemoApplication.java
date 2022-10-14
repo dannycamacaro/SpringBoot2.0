29676dac1d8db0c7ac8d307ba266647338b731eb
DESKTOP-F1DBC7V\Danny <DannyCamacaro>
2022-10-14 10:41:45 -0500

    When writing a Spring Boot-based application, most of those require all these annotations. This code can be simplified by using the @SpringBootApplication annotation instead.

--- chapter1/1-SpringbootUsingMaven/SpringbootMaven/com/camamonte/springbootmaven/DemoApplication.java
+++ chapter1/1-SpringbootUsingMaven/SpringbootMaven/com/camamonte/springbootmaven/DemoApplication.java
@@ -1,15 +1,11 @@
 package com.camamonte.springbootmaven;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.Arrays;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
         var ctx = SpringApplication.run(DemoApplication.class, args);
